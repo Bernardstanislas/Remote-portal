2ebf969d424532af8758240f66c9fdf7883975e2
Stanislas Bernard <bernardstanislas@gmail.com>
2014-12-18 15:03:39 +0100

    Improve portal controller

--- api/controllers/PortalController.js
+++ api/controllers/PortalController.js
@@ -8,47 +8,47 @@
 var portName = '/dev/cu.usbserial-A94JN1PL';
 
 var SerialPort = require("serialport").SerialPort;
-var serialPort = new SerialPort(portName, {
-  baudrate: 9600
-});
 
-var openPortal;
+function doOpen(res, sp) {
+  if (!sp) {
+    sp = new SerialPort(portName, {
+      baudrate: 9600
+    });
+  }
 
-function init() {
-  serialPort.on("open", function () {
-    openPortal = function(cb) {
-      serialPort.on('data', cb);
-      serialPort.write("trigger\n", function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
+  var response = "";
+  sp.on('data', function(data) {
+    response += data.toString();
+    if (response.match(/\n/)) {
+      if (response.match(/success/)) {
+        res.json({message: 'Success'}, 200);
+      } else if (response.match(/error/)) {
+        res.json({error: 'Arduino failed to open the portal'}, 500);
+      } else {
+        res.json({error: 'Arduino replied crap'}, 400);
+      }
+      response = "";
+      sp.close();
+    }
+  });
+
+  sp.open(function(error) {
+    if (error) {
+      res.json({error: 'Arduino not found'}, 404);
+      sp.close();
+    } else {
+      sp.write("trigger\n", function(err) {
+        if (err) {
+          res.json({error: 'Failed to talk with arduino'}, 503);
+          sp.close();
+        }
       });
     }
   });
 }
 
-function doTrigger(req, res) {
-  var response = "";
-  serialPort.on('data', function(data) {
-    response += data.toString();
-    if (response.match(/\n/)) {
-      if (response.match(/success/)) {
-        res.json({message: 'success'}, 200);
-      } else {
-        res.json({message: 'error'}, 500);
-      }
-      response = "";
-    }
-  });
-  serialPort.write("trigger\n", function(err, results) {
-    if (err) {
-      res.json({message: 'error'}, 500);
-    }
-  });
-}
-
 module.exports = {
-  open: function(req, res) {
-    //init();
-    doTrigger(req, res);
+  open: function(req, res, sp) {
+    doOpen(res, sp);
   }
 };
--- tests/controllers/PortalController.spec.js
+++ tests/controllers/PortalController.spec.js
@@ -2,18 +2,118 @@
  * Created by stan on 17/12/14.
  */
 
+var events = require('events');
+
 var PortalController = require('../../api/controllers/PortalController'),
     sinon = require('sinon'),
     assert = require('assert');
 
+/**
+ * Mocked SerialPort object
+ * Used to simulate the arduino behavior
+ * @type(Object)
+ */
+var SerialPort = function() {
+    this.close = sinon.spy();
+};
+SerialPort.prototype = new events.EventEmitter;
+
+/**
+ * SerialPort instance
+ * Will simulate the arduino calls and responses
+ * @type(SerialPort)
+ */
+var serialPort = new SerialPort();
+
+/**
+ * Stubbed res object
+ * @type({json: function})
+ */
+var res = {
+    json: sinon.stub()
+};
+
 describe('The Portal Controller', function () {
-    describe('when we call the trigger page', function () {
-        it ('should return a 500 error', function () {
-            var res = {
-                json: sinon.stub()
+    describe('when we call the open action with a bad port configured', function () {
+        it ('should return a 404 error with an explicit message and close the serial connection.', function () {
+            serialPort.close.reset();
+            res.json.reset();
+            serialPort.open = function(cb) {
+                var error = true;
+                cb(error);
             };
-            PortalController.open(null, res);
-            sinon.assert.calledWithMatch(res.json, { message: "error" }, 500);
+            PortalController.open(null, res, serialPort);
+            sinon.assert.calledWithMatch(res.json, { error: "Arduino not found" }, 404);
+            assert(serialPort.close.calledOnce);
+        });
+    });
+
+    describe('when we call the open action with a good port configured and a deaf arduino', function() {
+        it('should return a 503 error with an explicit message and close the serial connection.', function () {
+            serialPort.close.reset();
+            res.json.reset();
+            serialPort.open = function(cb) {
+                cb();
+            };
+            serialPort.write = function(message, cb) {
+                var error = true;
+                cb(error);
+            };
+            PortalController.open(null, res, serialPort);
+            sinon.assert.calledWithMatch(res.json, { error: "Failed to talk with arduino" }, 503);
+            assert(serialPort.close.calledOnce);
+        });
+    });
+
+    describe('when we call the open action with a good port configured and a weird talking arduino', function() {
+        it ('should return a 400 error with an explicit message and close the serial conection.', function() {
+            this.clock = sinon.useFakeTimers();
+            serialPort.close.reset();
+            serialPort.removeAllListeners();
+            res.json.reset();
+            serialPort.open = function(cb) {
+                cb();
+            };
+            serialPort.write = function(message, cb) {
+                cb();
+                setTimeout(function() {
+                    serialPort.emit('data', 'suc');
+                }, 1505);
+                setTimeout(function() {
+                    serialPort.emit('data', 'lol\n');
+                }, 1510);
+            };
+            PortalController.open(null, res, serialPort);
+
+            this.clock.tick(1600);
+            sinon.assert.calledWithMatch(res.json, { error: "Arduino replied crap" }, 400);
+            assert(serialPort.close.calledOnce);
+        });
+    });
+
+    describe('when we call the open action with a good port configured and a faulty arduino', function() {
+        it ('should return a 500 error with an explicit message and close the serial conection.', function() {
+            this.clock = sinon.useFakeTimers();
+            serialPort.close.reset();
+            serialPort.removeAllListeners();
+            res.json.reset();
+            serialPort.open = function(cb) {
+                cb();
+            };
+            serialPort.write = function(message, cb) {
+                cb();
+                setTimeout(function() {
+                    serialPort.emit('data', 'err');
+                }, 1505);
+                setTimeout(function() {
+                    serialPort.emit('data', 'or\n');
+                }, 1510);
+            };
+            PortalController.open(null, res, serialPort);
+
+            this.clock.tick(1600);
+            sinon.assert.calledWithMatch(res.json, { error: "Arduino failed to open the portal" }, 500);
+            assert(serialPort.close.calledOnce);
         });
     });
 });
